62f5f6d00e0eae04c157607078767e723df5ee19
BerkayAtakli <9023046@student.zadkine.nl>
2026-06-09 09:28:16 +0200

    .

--- Obstacle Dodge/Assets/Scripts/ObjectHit.cs
+++ Obstacle Dodge/Assets/Scripts/ObjectHit.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
+   
    private void OnCollisionEnter(Collision other)
    {
         GetComponent<Renderer>().material.color = Color.red;
-        Debug.Log("Something hit me!");
    }
 }
--- Obstacle Dodge/Assets/Scripts/Scorer.cs
+++ Obstacle Dodge/Assets/Scripts/Scorer.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class Scorer : MonoBehaviour
 {
-   private void OncollisionEnter(Collision other)
+   int hits = 0;
+   private void OnCollisionEnter(Collision other)
    {
-        Debug.Log("You've bumped into a thing this many times!");
+        hits++;
+        Debug.Log("You've bumped into a thing this many times: " + hits);
    }
 }
